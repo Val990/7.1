5682ae47fd921f779434f05781db6dc0c8553251
Valeriya <tmcxezsim5nn@mail.ru>
2021-10-03 18:44:19 +0300

    test modified

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -9,6 +9,8 @@ class StatisticsServiceTest {
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
+        long current_max;
+        long income;
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
